1571ecefeb55bad52f5b21ead8c748c0ca497dc8
Tatianaob <ltatiobandore@gmail.com>
2025-05-14 17:02:59 -0700

    Add helper methods for stack

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -17,3 +17,25 @@ class Stack:
         new_node.next = self.top
         self.top = new_node
         self._size += 1
+
+    def pop(self):
+        """Remove and return the top value"""
+        if self.is_empty():
+            raise IndexError("Pop from empty stack")
+        value = self.top.value
+        self.top = self.top.next
+        self._size -= 1
+        return self.top.value
+    
+    def peek(self):     
+        """Return the top value without removing it"""
+        if self.is_empty():
+            raise IndexError("Peek from empty stack")
+        return self.top.value
+
+    def is_empty(self):
+        return self.top is None
+    
+    def size(self):
+        return self._size
+
