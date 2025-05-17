d154ebb58a2015c502ccbe00cc6ef40563f69001
Tatianaob <ltatiobandore@gmail.com>
2025-05-16 21:47:20 -0700

    add method to search and sort nodes

--- data_structures/bst.py
+++ data_structures/bst.py
@@ -11,7 +11,7 @@ class BinarySearchTree:
     
     def insert(self, value):
         '''insert a value into the BST'''
-        self.root = self._insert_recuersive(self.root, value)
+        self.root = self._insert_recursive(self.root, value)
 
     def _insert_recursive(self, node, value):
         if node is None:
@@ -21,3 +21,28 @@ class BinarySearchTree:
         elif value > node.value:
             node.right = self._insert_recursive(node.right, value)
         return node
+    
+    def search(self, value):
+        return self._search_recursive(self.root, value)
+    
+    def _search_recursive(self, node, value):
+        if node is None:
+            return False
+        if value == node.value:
+            return True
+        elif value < node.value:
+            return self._search_recursive(node.left, value)
+        else:
+            return self._search_recursive(node.right, value)
+        
+    def inorder(self):
+        result = []
+        self._inorder_traversal(self.root, result)
+        return result
+    
+    def _inorder_traversal(self, node, result):
+        if node:
+            self._inorder_traversal(node.left, result)
+            result.append(node.value)
+            self._inorder_traversal(node.right, result)
+
