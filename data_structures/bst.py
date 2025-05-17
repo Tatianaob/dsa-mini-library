fa82fedb2d49d4a88449f601796650c460c9112a
Tatianaob <ltatiobandore@gmail.com>
2025-05-16 21:33:39 -0700

    add insert method

--- data_structures/bst.py
+++ data_structures/bst.py
@@ -8,4 +8,16 @@ class TreeNode:
 class BinarySearchTree:
     def __init__(self):
         self.root = None
-        
+    
+    def insert(self, value):
+        '''insert a value into the BST'''
+        self.root = self._insert_recuersive(self.root, value)
+
+    def _insert_recursive(self, node, value):
+        if node is None:
+            return TreeNode(value)
+        if value < node.value:
+            node.left = self._insert_recursive(node.left, value)
+        elif value > node.value:
+            node.right = self._insert_recursive(node.right, value)
+        return node
